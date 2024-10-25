7a7bc929d19af223ae9053fe552323641521d0ef
JokMaker <j.kur@alustudent.com>
2024-10-25 18:16:55 +0000

    done

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -11,7 +11,13 @@ def top_ten(subreddit):
     HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
     try:
         RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+    if RESPONSE.status_code != 200:
+            print(None)
+            return
         HOT_POSTS = RESPONSE.json().get("data").get("children")
+        if HOT_POSTS:
         [print(post.get('data').get('title')) for post in HOT_POSTS]
+        else:
+            print(None)
     except Exception:
         print(None)
