1f302735837e5bd78b842429bc6a427928ec3a0a
root <root@Jok.localdomain>
2024-10-25 17:22:37 +0200

    Afunction that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
-"""Prints the title of the first 10 hot posts listed for a given subreddit"""
-
+"""Print the titles of the first 10Hot Posts"""
 import requests
 
+
 def top_ten(subreddit):
-    """Main function"""
-    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+    """The top ten titles"""
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(url, headers=headers)
 
-    try:
-        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        
-        # Check if the request was successful
-        if RESPONSE.status_code != 200:
-            print(None)
-            return
-
-        HOT_POSTS = RESPONSE.json().get("data", {}).get("children", [])
-        
-        # Print the titles if available
-        if HOT_POSTS:
-            [print(post.get('data').get('title')) for post in HOT_POSTS]
-        else:
-            print(None)
-    except Exception:
+    if response.status_code == 200:
+        json_data = response.json()
+        for i in range(10):
+            print(
+                    json_data.get('data')
+                    .get('children')[i]
+                    .get('data')
+                    .get('title')
+                )
+    else:
         print(None)
